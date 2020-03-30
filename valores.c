d6fd4bed2ac5f1da4c9696506eccddb909b123df
4L3_xD <alessandra.olea@gmail.com>
2020-03-30 09:55:53 -0300

    update code

--- valores.c
+++ valores.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void main(){
-    int count, initialLim, limit, num, big, small;
-    float media;
+    int count, initialLim, limit, num, big, small = 0;
+    float media = 0;
     
     printf("\nVamos brincar com alguns valores!\n\nDigite um inteiro que represente a quantidade de números que você deseja inserir: ");
     scanf("%i", &limit);
@@ -10,17 +10,19 @@ void main(){
     printf("\nDigite %i números e pressione ENTER a cada entrada. \n", limit);
     
     initialLim = limit;
-    while (limit > 0){
-        for(int i = 1; count != initialLim; i++){
-            count++;
-            printf("\nDigite um valor: ");
-            scanf("%d", &num);
-            printf("\n Valor %i: %d\n", i, num);
-            if(num > big){
-                big = num;
-            }
-        limit--;
+    for(int i = 0; i < limit; i++){
+        printf("\nDigite um valor: ");
+        scanf("%d", &num);
+        printf("\n Valor %i: %d\n", i+1, num);
+        if(num > big){
+            big = num;
         }
+        if(num < small){
+            small = num;
+        }
+        int sum = 0;
+        sum = sum+num;
+        media = sum/initialLim;
     }
     printf("\nO maior número é: %d\n", big);
     printf("\nO menor número é: %d\n", small);
