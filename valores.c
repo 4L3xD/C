8fcc25f9eaa9c8e06ea18b6170b1a06b77b9c2ca
4L3_xD <alessandra.olea@gmail.com>
2020-03-30 09:39:27 -0300

    add count numbers

--- valores.c
+++ valores.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
 void main(){
-    int limite, num, big;
+    int count, initialLim, limit, num, big, small;
+    float media;
     
     printf("\nVamos brincar com alguns valores!\n\nDigite um inteiro que represente a quantidade de números que você deseja inserir: ");
-    scanf("%d", &limite);
+    scanf("%i", &limit);
 
-    printf("\nDigite %d números e pressione ENTER a cada entrada: \n", limite);
-    while (limite > 0){
+    printf("\nDigite %i números e pressione ENTER a cada entrada. \n", limit);
+    
+    initialLim = limit;
+    while (limit > 0){
+        for(int i = 1; count != initialLim; i++){
+            count++;
+            printf("\nDigite um valor: ");
             scanf("%d", &num);
+            printf("\n Valor %i: %d\n", i, num);
             if(num > big){
                 big = num;
-        } limite--;
+            }
+        limit--;
+        }
     }
-    printf("O maior número é %d\n", big);
+    printf("\nO maior número é: %d\n", big);
+    printf("\nO menor número é: %d\n", small);
+    printf("\nA média dos números é: %d\n", media);
+
 }
